485d4d6489c11b294de950a52a704b77b8ed1a60
MatejMarko <matej.mbssk@gmail.com>
2019-04-12 12:49:36 +0200

    task

--- task.py
+++ task.py
@@ -18,3 +18,37 @@
 #
 #Delete these comments before commit!
 #Good luck.
+
+import random as rd
+
+class Flight:
+
+	def __init__(self):
+		self.angle = 0
+
+	def correct_angle(self):
+		higher = True
+		if self.angle < 0:
+			higher = False
+		if higher:
+			self.angle += -(rd.random() * rd.randint(0, int(self.angle)))
+		else:
+			self.angle += -(rd.random() * rd.randint(int(self.angle), 0))
+	
+	def turbolation(self):
+		self.angle += rd.random() * rd.randint(-3, 3)
+
+	
+if __name__ == '__main__':
+		
+	flight = Flight() 
+	while(True):
+
+		flight.turbolation()
+
+		print("Flight angle is " + str(flight.angle))
+
+		flight.correct_angle()
+
+		print("Flight angle corrected to " + str(flight.angle))
+		
